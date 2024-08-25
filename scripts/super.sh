c9b56debfc65e31db9a77a9348d10a16723e05fd
super27035 <superssatellite@gmail.com>
2024-08-25 22:25:17 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -56,17 +56,12 @@ git clone --depth=1 https://github.com/super27035/luci-app-vssr
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
 # Add OpenClash
-# svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+# git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
+# cd OpenClash
+# git sparse-checkout set luci-app-openclash
 
 # Add OpenClash
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git
-# rm -rf OpenClash/img
-# rm -rf OpenClash/.github
-
-# Add OpenClash
-git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
-cd OpenClash
-git sparse-checkout set luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash
 
 # Add ddnsto & linkease
 # svn export https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
