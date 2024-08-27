b479b40fab8cc9e9428c1424cacd48357f4c3b39
super27035 <superssatellite@gmail.com>
2024-08-28 07:53:34 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -32,15 +32,10 @@ git sparse-checkout set luci-app-irqbalance
 
 
 # Add luci-app-passwall
-# mkdir passwall passwall2 passwall-packages 
-# git clone https://github.com/xiaorouji/openwrt-passwall passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall2 passwall2
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages
-
-# git clone https://github.com/xiaorouji/openwrt-passwall
-# git clone -b packages https://github.com/xiaorouji/openwrt-passwall
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall
-# svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+mkdir passwall passwall2 passwall-packages 
+git clone https://github.com/xiaorouji/openwrt-passwall passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
