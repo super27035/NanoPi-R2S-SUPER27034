c8c3071bcd03d9326240e7444615735fbc56717b
super27035 <superssatellite@gmail.com>
2024-08-30 01:05:08 +0800

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -8,8 +8,10 @@
 #=================================================
 
 mkdir -p files/etc/openclash/core
-
+# 这个 URL 是指向 vernesong 用户或组织在 GitHub 上的 OpenClash 仓库中，core 分支的 master/dev 
+# 目录下的 clash-linux-arm64.tar.gz 文件的原始内容。通过访问这个 URL，你可以直接下载该文件的最新版本。
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
+
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep -v "v3" )
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
